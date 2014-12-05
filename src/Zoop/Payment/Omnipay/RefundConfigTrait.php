4c851ad7f775af0ac7ca670559ebec5d15be03d5
dollyaswin <dolly.aswin@gmail.com>
2014-12-05 14:25:39 +0700

    Payment Refund
    Add some interfaces for Payment Refund

---
+++ src/Zoop/Payment/Omnipay/RefundConfigTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Zoop\Payment\Omnipay;
+
+trait RefundConfigTrait
+{
+    /**
+     * @return string
+     */
+    public function getTransactionReference()
+    {
+        return $this->transactionReference;
+    }
+
+    /**
+     * @param string $transactionReference
+     */
+    public function setTransactionReference($transactionReference)
+    {
+        $this->transactionReference = (string) $transactionReference;
+    }
+    
+    /**
+     * @param float $amount
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+    }
+    
+    /**
+     * @return float
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+}
