afba50775dfa7aa86f854fc87bdb6f3cf8fbf15e
crimsonronin <josh.stuart@zoopcommerce.com>
2014-12-24 17:10:26 +1100

    updated user roles

--- src/Zoop/User/Roles.php
+++ src/Zoop/User/Roles.php
@@ -5,8 +5,8 @@ namespace Zoop\User;
 class Roles
 {
     const SYSTEM_AUTH_USER = 'sys::auth-user';
-    const COMPANY_ADMIN = 'company::admin';
-    const CUSTOMER = 'customer';
+    const CUSTOMER_ADMIN = 'customer::admin';
+    const CONSUMER = 'consumer';
     const GUEST = 'guest';
     const PARTNER_ADMIN = 'partner::admin';
     const ZOOP_ADMIN = 'zoop::admin';
